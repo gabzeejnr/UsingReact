aaa3ace2480c483cbb69bf68e2513adfd6cd59f8
gabzeejnr <gabrieldodowei@gmail.com>
2026-06-21 12:33:03 +0100

    Add files via upload

---
+++ src/About.jsx
@@ -0,0 +1,2 @@
+export const Address = `7095 Indiana Ave #200, Riverside, CA 92506`;
+    
--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,18 +1,28 @@
+import companyLogo from '../assets/company-logo.png';
+
 function Footer() {
 
-    const footerStyle ={
-        background: "rgb(177, 151, 107)",
-        marginTop: "auto"
+    const imgStyle ={
+        background: "#transparent",
+        maxHeight: "70px",
+        maxWidth: "140px",
+        marginLeft: "-1rem",
     }
 
     return(
         <footer id="footer-wrap">
-            <div>Copyright &copy; John L. Michels {new Date().getFullYear()}.
+            <div style={{display: "flex", flexDirection: "column", gap: "1rem"}}>
+                <img src={companyLogo} style={imgStyle} alt="Our Company Logo" />
+
+                Copyright &copy; John L. Michels {new Date().getFullYear()}.
                 <br />
                 John L. Michels is a global law firm operating through various separate and distinct legal entities.
                 <br />
                 All Rights Reserved.</div>
-            <div>THis is a footer</div>
+            <div style={{display: "flex", flexDirection: "column", gap: "1rem"}}>
+                <div className="footer-right-top"></div>
+                <div className="footer-right-bottom"></div>
+            </div>
         </footer>
     )
 }
